36683dece5827a37eb8af42045f56fbf48e6bdcc
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-09-02 09:31:25 +0200

    Refactor initial code structure and add greeting message functionality

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -1,25 +1,23 @@
 /* Opgave 1*/
 
+// din kode her
+const navn = "Peter";
+const aarIvirksomhed = 5;
+const besked = `Hej ${navn} du har arbejdet i vores virksomehed i ${aarIvirksomhed} år.`;
+console.log(besked);
+const message = document.getElementById("message");
+message.innerHTML = besked;
+
+/* Opgave 2*/
 
 // din kode her
-
-
-/* Opgave 1*/
-
-
-// din kode her
-
-
 
 /* Opgave 3 - skriv videre på koden her: */
 
 // array med person data
 const myPersons = [];
 
-
 // find form elementer
-const myForm = document.getElementById('formular');
-const submitButton = document.getElementById('indsend-knap');
-const personList = document.getElementById('person-liste');
-
-
+const myForm = document.getElementById("formular");
+const submitButton = document.getElementById("indsend-knap");
+const personList = document.getElementById("person-liste");
